8e41a55689851080f04ced3d9756357a1ebc982e
viniciuslzs <vinicius.luiz.9256@gmail.com>
2022-03-21 16:43:20 -0300

    Add-menu-mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+function initMenuMobile(){
+const menuMobile = document.querySelector('#menu-mobile');
+
+function toggleMenu(){
+  const nav = document.querySelector('#nav');
+  nav.classList.toggle('active');
+}
+
+menuMobile.addEventListener('click', toggleMenu);
+}
+initMenuMobile();
